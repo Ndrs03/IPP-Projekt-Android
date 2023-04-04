fcf1a8c6c39092158e30a732b7312ff984838805
Djee03 <dennis.njordstorp@gmail.com>
2023-04-04 13:43:28 +0200

    non-null warning

--- app/src/main/java/com/example/ipp_android_app/FirstFragment.kt
+++ app/src/main/java/com/example/ipp_android_app/FirstFragment.kt
@@ -13,7 +13,7 @@ import com.example.ipp_android_app.databinding.FragmentFirstBinding
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null //yes
+    private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,7 +22,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
